78270a3fa079b7130e5740cede2b3f143f960deb
RolinShmily <RolinShmily@users.noreply.github.com>
2026-07-31 12:38:54 +0800

    fix: revert neovim installation to pkg for termux compatibility

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ log_info "开始安装 Termux 系统依赖包..."
 # 更新包列表
 pkg update -y
 
-TERMUX_PKGS="zsh eza zoxide fzf bat fd ripgrep diff-so-fancy gh lazygit jq tealdeer nodejs python uv yazi file chafa imagemagick poppler ffmpeg p7zip unar exiftool mediainfo miller git clang make unzip tree-sitter zellij fastfetch htop termux-api"
+TERMUX_PKGS="zsh eza zoxide fzf bat fd ripgrep diff-so-fancy gh lazygit jq tealdeer nodejs python uv yazi file chafa imagemagick poppler ffmpeg p7zip unar exiftool mediainfo miller neovim git clang make unzip tree-sitter zellij fastfetch htop termux-api"
 
 pkg install -y $TERMUX_PKGS
 log_success "Termux 系统软件包安装完成。"
@@ -70,18 +70,3 @@ fi
 
 log_success "所有依赖安装完成。请运行 ./config.sh 部署配置文件。"
 
-# 安装定制版 Neovim v0.11.7 (二进制)
-log_info "从 Github 下载并安装 Neovim v0.11.7 二进制版本..."
-NVIM_VERSION="v0.11.7"
-NVIM_URL="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-x86_64.tar.gz"
-NVIM_DIR="$HOME/.local/nvim-linux-x86_64"
-
-mkdir -p "$HOME/.local/bin"
-cd /tmp
-curl -L -O "$NVIM_URL"
-tar xzf nvim-linux-x86_64.tar.gz
-rm -rf "$NVIM_DIR"
-mv nvim-linux-x86_64 "$HOME/.local/"
-ln -sf "$NVIM_DIR/bin/nvim" "$HOME/.local/bin/nvim"
-rm -f /tmp/nvim-linux-x86_64.tar.gz
-log_success "Neovim v0.11.7 安装完成。请确保 ~/.local/bin 在 PATH 中。"
